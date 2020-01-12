e521d26e6dd7ed5ad008ca7ff8ac1a8f803ce6c3
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-01-12 12:34:06 -0700

    learned types of javascript snobnus

--- playground/some.js
+++ playground/some.js
@@ -3,3 +3,13 @@ console.log('im in another file');
 
 const age = 100;
 console.log(p, age);
+
+// SNOB'N'US
+// String
+// number
+// boolean
+// null
+// undefined
+// symbol
+
+// object -- everything in javascript is objects.
