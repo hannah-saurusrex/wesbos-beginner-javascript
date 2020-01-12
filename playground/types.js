79dde4cd6997938d88858ed14607b472e20db8b4
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-01-12 13:21:33 -0700

    played around with objects

--- playground/types.js
+++ playground/types.js
@@ -1,33 +1,74 @@
-/* eslint-disable */
+// /* eslint-disable */
+// const name = 'wes';
+// const middle = "topher";
+// const last = `bos`;
+
+// const sentence = 'she\'s so cool';
+// const sentence2 = `she's so cool`;
+
+// const song = `oooooh
+
+// y
+
+// i like
+
+// pizza;`
+
+// const hello = "hello my name is " + name + ". nice to meet you";
+
+// // don't do it this way. this is ridiculous.
+// // let hello2 = 'hello my name is ';
+// // hello2 = hello2 + name;
+// // hello2 = hello2 + ' nice to meet you';
+
+// const hello3 = `hello my name is ${name}. Nice to meet you. I am ${1 +100} years old.`;
+
+// const html = `
+//   <div>
+//   <h2>${name}</h2>
+//   <p>${hello}</p>
+//   </div>
+// `;
+
+// document.body.innerHTML = html;
+
+/*
+const age = 100;
 const name = 'wes';
-const middle = "topher";
-const last = `bos`;
 
-const sentence = 'she\'s so cool';
-const sentence2 = `she's so cool`;
+// const a = 10;
+// const b = 20;
 
-const song = `oooooh
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+const dadGets = smarties % kids; // how many left over after given to all kids; dad will get two.
+console.log(`each kid gets ${eachKidGets}`);
+*/
 
-y 
+// this is an object
+/*
+const person = {
+  first: 'wes',
+  last: 'bos',
+  age: 100,
+};
+*/
 
-i like
+let dog;
+console.log(dog); // this will show up as undefined in the console
 
-pizza;`
+let somethingUndefined; // no value
+const somethingNull = null; // only null if explicitly defined as null.
 
-const hello = "hello my name is " + name + ". nice to meet you";
+const cher = {
+  first: 'cher',
+};
 
-// don't do it this way. this is ridiculous.
-// let hello2 = 'hello my name is ';
-// hello2 = hello2 + name;
-// hello2 = hello2 + ' nice to meet you';
+const teller = {
+  first: 'raymond',
+  last: 'teller',
+};
 
-const hello3 = `hello my name is ${name}. Nice to meet you. I am ${1 +100} years old.`;
-
-const html = `
-  <div>
-  <h2>${name}</h2>
-  <p>${hello}</p>
-  </div>
-`;
-
-document.body.innerHTML = html;
+teller.first = 'Teller';
+teller.last = null; // legally changed last name to nothing.
