db4fc7383e79c1798110fdf671e360d5cdf246c4
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-01-14 13:30:43 -0700

    made a super condensed, tricky, too complicated arrow function

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -24,3 +24,34 @@
 /* eslint-disable */
 const inchToCM = (inches) => inches * 2.54; // this is an orrow function
   // as soon as the keyword `return` appears in a function, any code after that (inside the function) will not run. it will be undectable.
+
+// function add(a, b = 3) {
+//   const total = a + b;
+//   return total;
+// }
+
+/* convert the above function to an arrow function */
+// const add = (a, b = 3) => a + b;
+
+// returning an object
+
+// function makeABaby(first, last) {
+//   const baby = {
+//     name: `${first} ${last}`,
+//     age: 0
+//   }
+//   return baby;
+// }
+
+/* first iteration of an arrow function. this works great, and wes said you can stop here! */
+// const makeABaby = (first, last) => {
+//   const baby = {
+//     name: `${first} ${last}`,
+//     age: 0
+//   }
+//   return baby;
+// }
+
+/* let's take the arrow function a bit further and feel super fancy */
+const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
+/* this one is a bit complicated and tricky to understand. maybe we went too far...? (think about your future self!) */
