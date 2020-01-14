da4883fc120bcef3a62e111a8511a61e76b56e28
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-01-14 13:15:11 -0700

    played around with writing arrow functions

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -2,77 +2,25 @@
 //   return `Dr. ${firstName}`;
 // }
 
-// Anon Function
+// anonymous function
 // function (firstName) {
 //   return `Dr. ${firstName}`;
 // }
 
-// Function Expression
+// console.log(doctorize2('Hannah'));
+
+// function expression
 // const doctorize = function(firstName) {
 //   return `Dr. ${firstName}`;
 // };
 
+// functions declared with the `function` keyword are considered "hoisted". meaning javascript hoists them to the top of the page giving them priority.
+// function doctorize2(firstName) {
+//   return `Dr. ${firstName}`;
+// }
+
+// arrow functions offer concise syntax -- shorter. don't have their own scope.
+// arrow functions are anonymous functions.
 /* eslint-disable */
-const inchToCM = inches => inches * 2.54;
-
-// function add(a, b = 3) {
-//   const total = a + b;
-//   return total;
-// }
-
-// const add = (a, b = 3) => a + b;
-
-// returning an object
-
-// function makeABaby(first, last) {
-//   const baby = {
-//     name: `${first} ${last}`,
-//     age: 0
-//   }
-//   return baby;
-// }
-
-const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
-
-
-// IIFE
-// Immediately Invoked Function Expression
-
-(function(age) {
-  return `You are cool and age ${age}`;
-})(10);
-
-// Methods!!!
-const wes = {
-  name: 'Westopher Bos',
-  // Method!
-  sayHi: function() {
-    console.log(`Hey ${this.name}`);
-    return 'Hey Wes';
-  },
-  // Short hand Method
-  yellHi() {
-    console.log('HEY WESSSSS');
-  },
-  // Arrow function
-  wisperHi: () => {
-    console.log('hii wesss im a mouse');
-  }
-}
-
-// Callback Functions
-// Click Callback
-const button = document.querySelector('.clickMe');
-
-function handleClick() {
-  console.log('Great Clicking!!');
-}
-
-button.addEventListener('click', function() {
-  console.log('NIce Job!!!');
-});
-
-// Timer Callback
-setTimeout(() => {
-  console.log('DONE! Time to eat!');
-}, 1000);
+const inchToCM = (inches) => inches * 2.54; // this is an orrow function
+  // as soon as the keyword `return` appears in a function, any code after that (inside the function) will not run. it will be undectable.
