ec718b3e25b3d9f7ced33e2e6daf3aa622269d72
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-01-16 13:58:34 -0700

    played around with get element by id

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -4,3 +4,6 @@ console.log(p);
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
 console.log(item2Image);
+
+// document.querySelector & document.querySelectorAll are mainstream and most typical ways to grab elements from a page.
+// document.getElementById is older and not as commonly used.
