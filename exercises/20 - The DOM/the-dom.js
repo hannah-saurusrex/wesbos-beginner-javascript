c78670b34890b4dea04441ecb1b84091e335fde2
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-01-17 13:47:41 -0700

    played around with insert adjacent, and text content

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,9 +1,23 @@
 const p = document.querySelector('p');
 const imgs = document.querySelectorAll('.item img');
-console.log(p);
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
-console.log(item2Image);
+const heading = document.querySelector('h2');
 
+console.log(heading.textContent);
+console.log(heading.innerText);
+// set the textContent property on the element
+// heading.textContent = 'hannah is cool';
+// console.log(heading.textContent); // text content is newer, but gives contents of the element
+// console.log(heading.innerText); // inner text is aware of css styling
 // document.querySelector & document.querySelectorAll are mainstream and most typical ways to grab elements from a page.
 // document.getElementById is older and not as commonly used.
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent);
+
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+pizzaList.insertAdjacentText('afterbegin', '🍕');
