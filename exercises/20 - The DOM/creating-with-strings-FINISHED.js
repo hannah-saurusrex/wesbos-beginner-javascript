337eb15d69801c54566287fc102e4a31e7ec792a
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-01-21 12:41:26 -0700

    wrote HTML from strings

--- exercises/20 - The DOM/creating-with-strings-FINISHED.js
+++ exercises/20 - The DOM/creating-with-strings-FINISHED.js
@@ -2,13 +2,13 @@ const item = document.querySelector('.item');
 
 const width = 500;
 const src = `https://picsum.photos/${width}`;
-const desc = `Cute Pup <img onload="alert('HACKED')" src="https://picsum.photos/50"/>`;
+const desc = `cute pup src="https://picsum.photos/50"/ `;
 const myHTML = `
-  <div class="wrapper">
-    <h2>Cute ${desc}</h2>
-    <img src="${src}" alt="${desc}"/>
-  </div>
-`;
+<div class = "wrapper">
+  <h2>cute ${desc}</h2>
+  <img src="${src}" alt="${desc}"/>
+</div>`;
+// myHTML will remain a string until we dump it into the DOM.
 
 // turn a string into a DOM element
 const myFragment = document.createRange().createContextualFragment(myHTML);
