492b7fbf7f8fa3236f42934874fb765fd0a831fe
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-01-29 14:37:35 -0700

    wrote click event as arrow function

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -5,8 +5,12 @@ function handleClick() {
   console.log('it got clicked!');
 }
 
+const hooray = () => console.log('hooray!');
+// same function, but written as arrow function.
+// technically an anon function, but written in a variable.
+
 butts.addEventListener('click', handleClick);
-coolButton.addEventListener('click', handleClick);
+coolButton.addEventListener('click', hooray);
 
 // this is an anonymous function since there is no name associated with the function
 // 1. go get something "butts".
@@ -14,3 +18,8 @@ coolButton.addEventListener('click', handleClick);
 // 3. go ahead and do something. console.log it worked. or maybe you toggle on a style class.
 
 butts.removeEventListener('click', handleClick);
+// to remove, must reference event and reference to function. can't remove with an anonymous function.
+
+// binding -- handleClick function is bound to the butts element.
+// handleClick is also bound to the coolButton element.
+// in the removeEventLIstener, handleClick is unbound from the butts element.
