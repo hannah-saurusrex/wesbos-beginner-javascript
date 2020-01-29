c08847ce1e97a2702b4e33d36c6fedfc61e9ea07
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-01-29 14:32:04 -0700

    added main function to handle click events, and used variables to grab different parts of the doc to then use only one click event

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -1,5 +1,14 @@
 const butts = document.querySelector('.butts');
+const coolButton = document.querySelector('.cool');
 
-butts.addEventListener('click', function() {
+function handleClick() {
   console.log('it got clicked!');
-});
+}
+
+butts.addEventListener('click', handleClick);
+coolButton.addEventListener('click', handleClick);
+
+// this is an anonymous function since there is no name associated with the function
+// 1. go get something "butts".
+// 2. listen for something "click"
+// 3. go ahead and do something. console.log it worked. or maybe you toggle on a style class.
