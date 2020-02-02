9bfc63acb7370be4f71954b23c17952289ef7fe3
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-02 15:08:00 -0700

    wrote prevent default, link and page change event listener

--- exercises/29 - Events/forms.js
+++ exercises/29 - Events/forms.js
@@ -1,5 +1,11 @@
 const wes = document.querySelector('.wes');
 
 wes.addEventListener('click', function(event) {
-  console.log(event);
+  event.preventDefault();
+  const shouldChangePage = confirm(
+    'this website might be malicious, do you wish to proceed?'
+  );
+  if (shouldChangePage) {
+    window.location = event.currentTarget.href;
+  }
 });
