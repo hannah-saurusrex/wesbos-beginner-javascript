7503a6ebf17fe3016ac55f8dfb1d930f70f20502
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-03 18:22:27 -0700

    started etch-a-sketch exercise, and grabbed important variables

---
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -0,0 +1,15 @@
+// select the elements on the page -
+// canvas, shake button
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shake = document.querySelector('.shake');
+
+// setup our canvas for drawing
+
+// write a draw function
+
+// write a handler for the keys
+
+// clear / shake function
+
+// listen for arrow keys
