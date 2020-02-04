72ae7fc051a0caa746d6a0ef5215875717dc95f1
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-04 12:03:55 -0700

    created width and height variables and ctx lines

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -5,6 +5,22 @@ const ctx = canvas.getContext('2d');
 const shake = document.querySelector('.shake');
 
 // setup our canvas for drawing
+// const { width } = canvas;
+// const { height } = canvas;
+
+// make a variable called height & width from the same properties on our canvas.
+const { width, height } = canvas;
+
+// create random x & y starting points on the canvas.
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath(); // start the drawing
+ctx.moveTo(200, 200);
+ctx.lineTo(200, 200);
+ctx.stroke();
 
 // write a draw function
 
