1174de204e46cd6454c9391baf36f1e900b191be
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-06 16:53:06 -0700

    wrote function to handle the click event and show the image at a larger size, and the description

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,7 +1,21 @@
 const cardButtons = document.querySelectorAll('.card button');
+const modalInner = document.querySelector('.modal-inner');
+const modalOuter = document.querySelector('.modal-outer');
 
-function handleCardButtonClick() {
-  console.log('You clicked it!');
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest('.card');
+  // grab the image source
+  const imgSrc = card.querySelector('img').src;
+  console.log(imgSrc);
+  const desc = card.dataset.description;
+  const name = card.querySelector('h2').textContent;
+  // populate the modal with the new info
+  modalInner.innerHTML = `
+  <img src="${imgSrc.replace('200', '600')}" alt="${name}"/>
+  <p>${desc}</p>`;
+  // show the modal
+  modalOuter.classList.add('open');
 }
 
 cardButtons.forEach(button =>
