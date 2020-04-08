3a47ea6f958dbd4c67f5d4635593d6a1ee7cf839
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-04-08 15:46:12 -0600

    worked through the face detection exercise

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -4,14 +4,13 @@ const ctx = canvas.getContext('2d');
 const faceCanvas = document.querySelector('.face');
 const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
-const optionsInputs = document.querySelectorAll(
-  '.controls input[type="range"]'
-);
+const optionsInputs = document.querySelectorAll('.controls input[type="range"]');
+console.log(optionsInputs);
 
 const options = {
   SIZE: 10,
   SCALE: 1.35,
-};
+}
 
 function handleOption(event) {
   const { value, name } = event.currentTarget;
@@ -19,14 +18,14 @@ function handleOption(event) {
 }
 optionsInputs.forEach(input => input.addEventListener('input', handleOption));
 
-// Write a fucntion that will populate the users video
+// write a function that will populate a user's video
 async function populateVideo() {
   const stream = await navigator.mediaDevices.getUserMedia({
-    video: { width: 1280, height: 720 },
+    video: { width: 1280, height: 720 }
   });
   video.srcObject = stream;
   await video.play();
-  // size the canvases to be the same size as the video
+  // size the canvasas to be the same size as the video
   console.log(video.videoWidth, video.videoHeight);
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
@@ -34,6 +33,7 @@ async function populateVideo() {
   faceCanvas.height = video.videoHeight;
 }
 
+// activate face detection
 async function detect() {
   const faces = await faceDetector.detect(video);
   // ask the browser when the next animation frame is, and tell it to run detect for us
@@ -42,6 +42,7 @@ async function detect() {
   requestAnimationFrame(detect);
 }
 
+// grab the face and put it in a square
 function drawFace(face) {
   const { width, height, top, left } = face.boundingBox;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -50,38 +51,38 @@ function drawFace(face) {
   ctx.strokeRect(left, top, width, height);
 }
 
+// pixelate a user's face on top of the stroked area
 function censor({ boundingBox: face }) {
   faceCtx.imageSmoothingEnabled = false;
-  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+  faceCtx.clearRect(0,0, faceCanvas.width, faceCanvas.height);
   // draw the small face
   faceCtx.drawImage(
-    // 5 source args
+    // 5 source arguments
     video, // where does the source come from?
     face.x, // where do we start the source pull from?
     face.y,
     face.width,
     face.height,
-    // 4 draw args
-    face.x, // where should we start drawing the x and y?
+    // 4 draw arguments
+    face.x, // where do we start drawing the x & y
     face.y,
     options.SIZE,
     options.SIZE
   );
-  // draw the small face back on, but scale up
-
+  // draw the small face back on, but scaled up
   const width = face.width * options.SCALE;
   const height = face.height * options.SCALE;
   faceCtx.drawImage(
     faceCanvas, // source
-    face.x, // where do we start the source pull from?
+    face.x,
     face.y,
     options.SIZE,
     options.SIZE,
-    // Drawing args
+    // drawing arguments
     face.x - (width - face.width) / 2,
     face.y - (height - face.height) / 2,
     width,
-    height
+    height,
   );
 }
 
