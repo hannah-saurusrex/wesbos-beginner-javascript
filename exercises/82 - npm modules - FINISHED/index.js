0b606423450b2bc4f33e21eccf6bc476e7649314
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-05-22 14:29:51 -0600

    worked through adding some npm packages and working with examples of how to use them

--- exercises/82 - npm modules - FINISHED/index.js
+++ exercises/82 - npm modules - FINISHED/index.js
@@ -1,11 +1,25 @@
 import wait from 'waait';
-import faker from 'faker'; // gives you fake data for testing purposes
+import { name } from 'faker'; // gives you fake data for testing purposes
+import { formatDistance } from 'date-fns'; // import a segment of the big library
 
-console.log(`Hello ${faker.name.firstName()}`);
+// console.log(`Hello ${name.firstName()}`);
+
+const fakeNames = Array.from(
+  { length: 10 },
+  () => `${name.firstName()} ${name.lastName()}`
+);
+// console.log(fakeNames);
 
 async function go() {
   console.log('going!');
   await wait(200);
   console.log('ending');
 }
-go();
+// go();
+
+const diff = formatDistance(
+  new Date(1986, 3, 4, 11, 32, 0),
+  new Date(1986, 3, 4, 10, 32, 0),
+  { addSuffix: true }
+); //= > 'in about 1 hour'
+console.log(diff);
